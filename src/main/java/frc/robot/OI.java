fbaf09e1108efde4353e59d0b38044bbb618fa92
Mustafa Miyaziwala <mustafa_miyazi@hotmail.com>
2019-02-06 19:10:07 -0800

    OI and Stuff

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,36 +7,58 @@
 
 package frc.robot;
 
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.ElevatorManualAscent;
+import frc.robot.commands.ElevatorManualDescent;
+import frc.robot.commands.ElevatorToPosition;
+import frc.robot.commands.ElevatorToPosition.ElevatorPositions;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
+	public Joystick driveStick;
+	public Joystick secondStick;
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
+	public enum Button {
+		RBumper(6), LBumper(5), A(1), B(2), X(3), Y(4), RightJoystickBtn(10), LeftJoystickBtn(9);
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
+		private final int number;
 
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
+		Button(int number) {
+			this.number = number;
+		}
 
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
+		public int getBtnNumber() {
+			return number;
+		}
+	}
 
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+	public enum Axis {
+		LX(0), LY(1), LTrigger(2), RTrigger(3), RX(4), RY(5);
+		private final int number;
+
+		Axis(int number) {
+			this.number = number;
+		}
+
+		public int getAxisNumber() {
+			return number;
+		}
+	}
+
+	public OI() {
+		driveStick = new Joystick(0);
+    secondStick = new Joystick(1);
+    
+    
+    new JoystickButton(driveStick, Button.LBumper.getBtnNumber()).whenPressed(new ElevatorManualAscent(0.9)); 
+    new JoystickButton(driveStick, Button.RBumper.getBtnNumber()).whenPressed(new ElevatorManualDescent(0.9)); 
+    new JoystickButton(driveStick, Button.A.getBtnNumber()).whenPressed(new ElevatorToPosition(ElevatorPositions.CARGO_HOLD));
+    new JoystickButton(driveStick, Button.B.getBtnNumber()).whenPressed(new ElevatorToPosition(ElevatorPositions.ROCKET_SECOND));
+    new JoystickButton(driveStick, Button.Y.getBtnNumber()).whenPressed(new ElevatorToPosition(ElevatorPositions.ROCKET_THIRD));
+	}
 }
--- src/main/java/frc/robot/commands/ElevatorManualDescent.java
+++ src/main/java/frc/robot/commands/ElevatorManualDescent.java
@@ -8,37 +8,51 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.OI;
+import frc.robot.Robot;
 
 public class ElevatorManualDescent extends Command {
-  public ElevatorManualDescent() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
+
+  private double power;
+
+  /**
+   * @return specify a positive power to make the elevator go down
+   * @param power
+   */
+  
+  public ElevatorManualDescent(double power) {
+    requires(Robot.elevator);
+    this.power = power;
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    Robot.elevator.setPower(0);
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.elevator.setPower(-power);
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return !Robot.oi.driveStick.getRawButton(OI.Button.LBumper.getBtnNumber());
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.elevator.setPower(0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    end();
   }
 }
